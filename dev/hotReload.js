9be3b6509ef35d4a386dd4eca696e51cce18db95
Kabir Shah <simplecooldude1@gmail.com>
2017-07-08 10:29:52 -0700

    fix hot reloading when render changes, and hot reloading when a script changes

--- dev/hotReload.js
+++ dev/hotReload.js
@@ -35,6 +35,7 @@ module.exports.reload = function(name, options, render) {
     var render = options.render;
     for(var i = 0; i < instances.length; i++) {
       var instance = instances[i];
+      instance.$dom = {};
       instance.$render = render;
       instance.build();
     }
@@ -42,35 +43,70 @@ module.exports.reload = function(name, options, render) {
     var oldCTor = item.CTor;
     var newCTor = Moon.component(name, options);
     var el = null;
+    var rootInstance = null;
+    var dom = null;
+    var meta = null;
+    var children = null;
+    var render = null;
 
     item.CTor = newCTor;
 
-    for(var i = 0; i < instances.length; i++) {
-      var instance = instances[i];
+    var reloadInstance = function(instance) {
+      var el = instance.$el;
+      el.__moon__ = undefined;
 
-      el = instance.$el;
-      delete el.__moon__;
-    }
-
-    while(el.__moon__ === undefined || el.__moon__ === instance) {
       el = el.parentNode;
-    }
-
-    var rootInstance = el.__moon__;
-    var dom = [rootInstance.$dom];
-
-    while(dom.length !== 0) {
-      var vnode = dom.pop();
-      if(vnode.meta.component !== undefined && vnode.meta.component.CTor === oldCTor) {
-        var componentInfo = vnode.meta.component;
-        componentInfo.CTor = newCTor;
-        componentInfo.options = options;
-        dom = [];
-      } else if(vnode.children.length !== 0) {
-        dom = dom.concat(vnode.children);
+      while(el.__moon__ === undefined) {
+        el = el.parentNode;
       }
+
+      rootInstance = el.__moon__;
+      dom = [rootInstance.$dom];
+
+      while(dom.length !== 0) {
+        var vnode = dom.pop();
+        if((componentInfo = (meta = vnode.meta).component) !== undefined && componentInfo.CTor === oldCTor) {
+          componentInfo.CTor = newCTor;
+          componentInfo.options = options;
+          dom = [];
+        } else if((children = vnode.children).length !== 0) {
+          dom = dom.concat(children);
+        }
+      }
+
+      render = rootInstance.$render;
+      rootInstance.$render = function(m) {
+        var dom = render(m);
+        var meta = null;
+        var children = null;
+        var componentInfo = null;
+
+        var update = function(vnodes) {
+          for(var i = 0; i < vnodes.length; i++) {
+            var vnode = vnodes[i];
+
+            if((componentInfo = (meta = vnode.meta).component) !== undefined && componentInfo.CTor === newCTor) {
+              meta.shouldRender = true;
+            }
+
+            if((children = vnode.children).length !== 0) {
+              update(children);
+            }
+
+          }
+        }
+
+        update(dom.children);
+
+        this.$render = render;
+        return dom;
+      }
+
+      rootInstance.build();
     }
 
-    rootInstance.build();
+    for(var i = 0; i < instances.length; i++) {
+      reloadInstance(instances[i]);
+    }
   }
 }
